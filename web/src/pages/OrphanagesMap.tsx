c8cc8756697184920e9dccd1be838193228397ca
Paulohfreire <paulo.freire53@gmail.com>
2020-10-15 00:06:27 -0300

    wip

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiPlus, FiArrowRight } from "react-icons/fi";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "../styles/pages/orphanages-map.css";
 import mapMarkerImg from "../images/marker.svg";
 import MapIcon from "../utils/mapIcon";
+import api from '../services/api';
 
+
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
 function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get('orphanages').then(response => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
   return (
     <div id="page-map">
       <aside>
@@ -31,17 +47,25 @@ function OrphanagesMap() {
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
 
-        <Marker 
+        {orphanages.map(orphanage => {
+          return (
+            <Marker 
+          key={orphanage.id}
           icon={MapIcon}
-          position={[-5.875709, -35.205203]}
+          position={[orphanage.latitude, orphanage.longitude]}
         > 
         <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
-          Praça da Independência
-          <Link to="/orphanages/1">
+        {orphanage.name}
+        <Link to={`/orphanages/${orphanage.id}`}>
             <FiArrowRight size={20} color="FFF" />
           </Link >
         </Popup>
         </Marker>
+
+          )
+        })}
+        
+
       </Map>
       <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
